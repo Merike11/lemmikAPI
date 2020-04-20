074d670a4f16c82853554c35d4ac521deb4c7f92
Merike11 <merike.toose@ametikool.ee>
2020-04-20 17:03:25 +0300

    Pildi kuvamine OK

--- read.php
+++ read.php
@@ -23,13 +23,22 @@ if (isset($_POST['submit'])) {
   }
 }
 ?>
-<?php require "templates/header.php"; ?>
 
+<?php require "templates/header.php"; ?>
+<div class="formarea ml-3" > 
+<h2>Leia maal</h2>
+
+<form method="post">
+  <label for="name">Nimetus:</label>
+  <input type="text" id="title" name="title">
+  <input type="submit" name="submit" value="Kuva tulemus">
+</form>
+<br>
 <?php
 if (isset($_POST['submit'])) {
   if ($result && $statement->rowCount() > 0) { ?>
  
-      <h2>Maalid</h2>
+      <h4>Maal:</h4>
 
       <table>
         <thead>
@@ -55,29 +64,19 @@ if (isset($_POST['submit'])) {
         </tr>
        
     <div class="img-block">
-        <img src="<?php echo $img_src; ?>" alt="" title="<?php echo $title; ?>" width="300" height="200" class="img-responsive" />
+        <img src="<?php echo $row["image"]; ?>" alt="" title="<?php echo $title; ?>" width="200" height="300" class="img-responsive" />
         <p><strong><?php echo $title; ?></strong></p>
     </div>         
       <?php } ?>
         </tbody>
     </table>
-    <h4>Siia saab tulema pilt sellest maalist, kunagi!!</h4>
-    <?php echo '<img src="'.$image.'" /><br />'; ?>
-
+    
 
     <?php } else { ?>
       > No results found for <?php echo escape($_POST['title']); ?>.
     <?php }
   } ?>
-<div class="formarea ml-3" > 
-  <h2>Leia maal</h2>
 
-  <form method="post">
-    <label for="name">Nimetus:</label>
-    <input type="text" id="title" name="title">
-    <input type="submit" name="submit" value="Kuva tulemus">
-  </form>
-  <br>
 <a href="index.php">Tagasi algusesse</a>
 
 <?php require "templates/footer.php";?>
